e2e2ac2cdbbf04049196317620ff68959c729f2d
Aksel Viktor <akselviktorrae@hotmail.com>
2025-08-21 11:53:50 -0300

    Update LeakySubscriber.cs

--- GCLab/LeakySubscriber.cs
+++ GCLab/LeakySubscriber.cs
@@ -1,16 +1,22 @@
-﻿namespace GCLab;
-
+namespace GCLab;
+ 
 class LeakySubscriber
 {
     private static readonly List<LeakySubscriber> _registry = new();
     private Publisher _publisher;
-
+ 
     public LeakySubscriber(Publisher publisher)
     {
         _publisher = publisher;
         _publisher.OnSomething += Handle;
         _registry.Add(this);
     }
-
+ 
     private void Handle() { /* noop */ }
+ 
+    public void Dispose()
+    {
+        _publisher.OnSomething -= Handle;
+ 
+    }
 }
